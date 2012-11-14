b6894ad4bb6843e1a10b72109c70e4402430c103
Alessandro Ambrosano <alessandro.ambrosano@gmail.com>
2012-11-14 14:23:34 +0100

    Spring cleaning

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 import sublime, sublime_plugin
 import pymsasid.pymsasid as pydasm
-import os
-import view
 
 class DecompileCommand(sublime_plugin.TextCommand):
-    def hurr_durr_buffehr(self, file_name):
-        buf = []
-        with open(file_name, "rb") as bin:
-            b = bin.read(1)
-            while b:
-                buf.append(b)
-                b = bin.read(1)
-
-        return buf
-
-    def hurr_durr_printor(self, inst, prog):
+    def asm_print(self, inst, prog):
         branch = inst.branch()
         while (len(branch) == 1):
             s = '[' + hex (prog.pc) + '] '
             inst = prog.disassemble (branch[0])
-            self.culo.write(s + str (inst) + '\n')
+            self.asm_out.write(s + str (inst) + '\n')
             print (s + str (inst))
             branch = inst.branch()
 
@@ -27,20 +15,21 @@ class DecompileCommand(sublime_plugin.TextCommand):
             if branch[0] not in self.branches:
                 self.branches.append(branch[0])
                 print "New Branch L"
-                self.culo.write("New Branch L\n")
+                self.asm_out.write("New Branch L\n")
                 self.hurr_durr_printor(prog.disassemble(branch[0]), prog)
             if branch[1] not in self.branches:
                 self.branches.append(branch[1])
                 print "New Branch R"
-                self.culo.write("New Branch R\n")
+                self.asm_out.write("New Branch R\n")
                 self.hurr_durr_printor(prog.disassemble(branch[1]), prog)
 
     def run(self, edit):
         self.branches = []
-        self.culo = open(os.path.dirname(self.view.file_name())+'\disasm', 'w')
+        self.asm_out = open(os.path.dirname(self.view.file_name())+'\\asm.out', 'w')
         prog = pydasm.Pymsasid(hook=pydasm.PEFileHook, source=self.view.file_name())
         inst = prog.disassemble(prog.pc)
-        app = QtCore.CoreApplication([])
-        launch_process()
-        app.exec_()
-        #self.hurr_durr_printor(inst, prog)
+        self.asm_print(prog, inst)
+        close(self.asm_out)
+        # app = QtCore.CoreApplication([])
+        # launch_process()
+        # app.exec_()
